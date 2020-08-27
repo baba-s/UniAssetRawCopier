0e27780824bd6a56c2d0a9fd8ec40d6d36d4d1d2
baba_s <shotababa1206@gmail.com>
2020-08-27 10:31:24 +0900

    first commit

---
+++ Editor/AssetRawCopier.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using UnityEditor;
+
+namespace Kogane.Internal
+{
+	internal static class AssetRawCopier
+	{
+		private const string ITEM_NAME = "Assets/Copy Raw";
+
+		[MenuItem( ITEM_NAME )]
+		private static void Copy()
+		{
+			var asset = Selection.activeObject;
+			var path  = AssetDatabase.GetAssetPath( asset );
+			var text  = File.ReadAllText( path );
+
+			EditorGUIUtility.systemCopyBuffer = text;
+		}
+
+		[MenuItem( ITEM_NAME, true )]
+		private static bool CanCopy()
+		{
+			var asset = Selection.activeObject;
+			var path  = AssetDatabase.GetAssetPath( asset );
+
+			if ( string.IsNullOrWhiteSpace( path ) ) return false;
+			if ( !File.Exists( path ) ) return false;
+
+			return true;
+		}
+	}
+}
